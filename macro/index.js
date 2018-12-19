1d35a4ae33bb18130283df5041de2bf1196b02f2
glortho <jed@dataverse.me>
2018-12-19 14:30:58 -0500

    fix path resolution; add react format support

--- macro/index.js
+++ macro/index.js
@@ -1,4 +1,4 @@
-fs   = require('fs');
+const fs   = require('fs');
 const path = require('path');
 const css  = require('css');
 
@@ -8,71 +8,104 @@ module.exports = createMacro(styleImportMacro);
 
 function styleImportMacro({references, state, babel}) {
   references.default.forEach(referencePath => {
-    const [ cssPathArg, selectorArg ] = referencePath.parentPath.get('arguments');
-    const cssRules = parseAst(state.file.opts.filename, cssPathArg.node.value).stylesheet.rules;
+
+    const { node } = referencePath.parentPath;
+
+    const format = node.property
+      ? node.property.name
+      : 'string'
+
+    const [ cssPathArg, selectorArg ] = node.property
+      ? referencePath.parentPath.parent.arguments
+      : referencePath.parentPath.get('arguments').map( arg => arg.node )
+
+    const cssRules = parseAst(state.file.opts.filename, cssPathArg.value).stylesheet.rules;
+
+    const declarationMethods = getDeclarationMethods(cssRules, babel, { format });
+
     const declarations = (
-      selectorArg.node.value ?
-        parseStringOfSelectors(cssRules, selectorArg.node.value, babel) :
-      selectorArg.node.elements ?
-        parseArrayOfSelectors(cssRules, selectorArg.node.elements, babel) :
-      parseObjectOfSelectors(cssRules, selectorArg.node.properties, babel)
+      selectorArg.value ?
+        declarationMethods.byString(selectorArg.value) :
+      selectorArg.elements ?
+        declarations.byArray(selectorArg.elements) :
+      declarationMethods.byObject(selectorArg.properties)
     )
-    const fnCallPath = cssPathArg.parentPath;
+
+    const fnCallPath = node.property
+      ? referencePath.parentPath.parentPath
+      : referencePath.parentPath
     fnCallPath.replaceWith(declarations);
   });
 }
 
-const stringifyDeclarations = declarations =>
-  declarations.map(item => `${item.property}: ${item.value}`).join('\n') + '\n'
+const getDeclarationMethods = (cssRules, babel, options = {}) => {
 
-const parseArrayOfSelectors = (cssRules, elements, babel) => {
-  const declarations = cssRules.map( rule => {
-    const ruleExists = rule.selectors.some( selector => !!elements.find(node => node.value === selector) )
-    return ruleExists
-      ? babel.types.stringLiteral(stringifyDeclarations(rule.declarations))
-      : babel.types.nullLiteral()
-  })
-  return babel.types.ArrayExpression(declarations)
-}
+  const stringifyDeclarations = declarations =>
+    declarations.map(item => `${item.property}: ${item.value}`).join('\n') + '\n'
 
-const parseObjectOfSelectors = (cssRules, properties, babel) => {
-  const declarations = properties.map(item => {
-    const key = item.key.name;
-    const value = item.value.value;
-    const rule = cssRules
-      .find( rule => rule.selectors.some( selector => selector === value ))
-    return babel.types.objectProperty(
-      babel.types.stringLiteral(key), (
-        rule
-          ? babel.types.stringLiteral(stringifyDeclarations(rule.declarations))
-          : babel.types.nullLiteral()
-      )
-    )
-  });
-  return babel.types.ObjectExpression(declarations)
-}
+  const objectifyDeclarations = declarations =>
+    declarations.map(item => babel.types.objectProperty(
+      babel.types.stringLiteral(toCamelCase(item.property)),
+      babel.types.stringLiteral(item.value)
+    ))
 
-const parseStringOfSelectors = (cssRules, value, babel) => {
-  const rules = cssRules.find( rule => rule.selectors.some( selector => selector === value ))
-  if (rules) {
-    const declarations = stringifyDeclarations(rules.declarations)
-    return babel.types.stringLiteral(declarations)
-  } else {
-    return babel.types.nullLiteral()
+  const formatDeclarations = rule => options.format === 'string'
+    ? babel.types.stringLiteral(stringifyDeclarations(rule.declarations))
+    : babel.types.ObjectExpression(objectifyDeclarations(rule.declarations))
+
+  const byArray = elements => {
+    const declarations = cssRules.map( rule => {
+      const ruleExists = rule.selectors.some( selector => !!elements.find(node => node.value === selector) )
+      return ruleExists
+        ? formatDeclarations(rule)
+        : babel.types.nullLiteral()
+    })
+    return babel.types.ArrayExpression(declarations)
   }
+
+  const byObject = properties => {
+    const declarations = properties.map(item => {
+      const key = item.key.name;
+      const value = item.value.value;
+      const rule = cssRules
+        .find( rule => rule.selectors.some( selector => selector === value ))
+      return babel.types.objectProperty(
+        babel.types.stringLiteral(key), (
+          rule
+            ? formatDeclarations(rule)
+            : babel.types.nullLiteral()
+        )
+      )
+    });
+    return babel.types.ObjectExpression(declarations)
+  }
+
+  const byString = value => {
+    const rule = cssRules.find( rule => rule.selectors.some( selector => selector === value ))
+    return rule
+      ? formatDeclarations(rule)
+      : babel.types.nullLiteral()
+  }
+
+  return { byString, byArray, byObject }
 }
 
 const parseAst = (modulePath, cssPath) => {
   // TODO: require.resolve?
   const pathHint = modulePath.charAt(0)
-  const pathReference = (
-    pathHint === '/' ?
-      path.dirname(modulePath) :
-    pathHint === '.' ?
-      path.join(process.cwd(), path.dirname(modulePath)) :
-    path.join(process.cwd(), 'node_modules', path.dirname(modulePath))
+  const pathReference = pathHint === '/'
+    ? path.dirname(modulePath)
+    : path.join(process.cwd(), path.dirname(modulePath))
+  const cssPathHint = cssPath.charAt(0)
+  const cssPathReference = (
+    cssPathHint === '/' ?
+      cssPath :
+    cssPathHint === '.' ?
+      path.join(pathReference, cssPath) :
+    require.resolve(cssPath)
   )
-  const absolutePath = path.join(pathReference, cssPath)
-  const cssString = fs.readFileSync(absolutePath, 'utf-8')
-  return css.parse(cssString, {source: absolutePath})
+  const cssString = fs.readFileSync(cssPathReference, 'utf-8')
+  return css.parse(cssString, {source: cssPathReference})
 }
+
+const toCamelCase = str => str.replace(/(-.)/g, chars => chars[1].toUpperCase())
